a72773cf992a365b9b7f64955053aa38476871b4
akuan <44944215+likuan123@users.noreply.github.com>
2026-06-26 12:12:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from PySide6.QtWidgets import QApplication
 
 from app.gui.main_window import MainWindow
 
+
 def main():
 
     app = QApplication(sys.argv)
+
+    app.setApplicationName("WhisperLRC Pro")
+
+    app.setOrganizationName("WhisperLRC")
 
     window = MainWindow()
 
